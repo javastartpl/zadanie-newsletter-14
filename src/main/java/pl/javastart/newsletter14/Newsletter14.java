cbb30a924edfa40974c84f57653793e94b1e022c
slawekludwiczak <slawek@javastart.pl>
2019-06-12 12:14:06 +0200

    Newsleter14

---
+++ src/main/java/pl/javastart/newsletter14/Newsletter14.java
@@ -0,0 +1,16 @@
+package pl.javastart.newsletter14;
+
+class Newsletter14 {
+    public static void main(String[] args) {
+        System.out.println(tricky());
+        //code here
+    }
+
+    private static Object tricky() {
+        //code here
+        Byte value = 127;
+        return value;
+        //code here
+    }
+
+}
